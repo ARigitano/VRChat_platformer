6fcd86021259fc27f5a0480f877db5f70612f8fa
ARigitano <antoine@rigitano.com>
2025-03-05 22:55:01 +0100

    Falling respawns the players

---
+++ Assets/Scripts/RespawnNet.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RespawnNet : UdonSharpBehaviour
+{
+    [SerializeField]
+    private Transform _spawnPoint; //Position at which the player will be respawned.
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        player.TeleportTo(_spawnPoint.position, Quaternion.identity);
+    }
+}
