f2155f05324ca9477d2e5cf8500fdf262ea2a73b
ARigitano <antoine@rigitano.com>
2025-04-17 17:20:21 +0200

    Weapon & Mob classes

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Mob : UdonSharpBehaviour
+{
+    private int _health = 10;
+    private int _damageTaken = 0;
+
+    public void TakingDamageActivate(int damageTaken)
+    {
+        damageTaken = _damageTaken;
+        TakingDamage(); //To replace with a network event.
+    }
+
+    private void TakingDamage()
+    {
+        _health -= _damageTaken;
+
+        if(_health <= 0)
+        {
+            Destroy(gameObject); //To replace with death animation.
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Weapon : UdonSharpBehaviour
+{
+    private int _damageAmount = 5;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Mob>() != null)
+        {
+            collision.gameObject.GetComponent<Mob>().TakingDamageActivate(_damageAmount);
+            Debug.Log("hit");
+            //Playing weapon sound
+        }
+    }
+}
